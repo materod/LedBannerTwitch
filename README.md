8dcd74e6c6de05669555cd2cf3aede286d630c5f
Maria Ten Rodriguez <materod@upvnet.upv.es>
2022-11-18 16:11:44 +0100

    Added login user

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# LebBannerTwitch
+# Led Banner Twitch Tool
+
+## Run local app
+
+<code> npm run build </code>
+
+## How to host app in Firebase
+
+1. Log in on https://console.firebase.google.com/ and select _Create new proyect_.
+2. Follow the wizard to create a new project.
+3. Select the project and then, select _Add new app_ of web type. Follow the wizard. The firebase dependencies are already included in the project so, you don't have to install them. Only copy de content of firebaseConfig object into a into _config/firebaseConfig.js_ file.
+4. Activar la autenticación a través de Firebase (ej: email/pass).
+5. Create a firebase database to store the user data.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
-console.log('hello from index.j');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+// Import your web app's Firebase configuration
+import { firebaseConfig } from '../config/firebaseConfig';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Authentication
+export const auth = getAuth(app);
+
+// Register new users
+const registerForm = document.querySelector('#registerForm');
+registerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = registerForm.registerEmail.value;
+  const pass = registerForm.registerPassword.value;
+  const pass2 = registerForm.registerRepeatPassword.value;
+
+  if (pass != pass2) {
+    registerForm.registerRepeatPassword.classList.add('is-invalid');
+  } else {
+    registerForm.registerRepeatPassword.classList.add('is-valid');
+
+    createUserWithEmailAndPassword(auth, email, pass)
+      .then((cred) => {
+        registerForm.registerPassword.classList.add('is-valid');
+        registerForm.reset();
+      })
+      .catch((err) => {
+        console.log(err.message);
+        registerForm.registerPassword.classList.add('is-invalid');
+      });
+  }
+});
+
+// Login
+const loginForm = document.querySelector('#loginForm');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = loginForm.loginName.value;
+  const pass = loginForm.loginPassword.value;
+
+  signInWithEmailAndPassword(auth, email, pass)
+    .then((cred) => {
+      loginForm.loginPassword.classList.add('is-valid');
+      loginForm.reset();
+    })
+    .catch((err) => {
+      console.log(err.message);
+      loginForm.loginPassword.classList.add('is-invalid');
+    });
+});
+
+// Logout
+const logoutButton = document.querySelector('#logoutBtn');
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {})
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// Auth changes
+onAuthStateChanged(auth, (user) => {
+  console.log('user state changed: ', user);
+});
